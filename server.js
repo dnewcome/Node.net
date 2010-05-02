786e4f53044b4ec8343f2bec631f576caf331e86
dnewcome <djn125@yahoo.com>
2010-05-02 00:56:57 -0400

    callback function and basic http response working

--- server.js
+++ server.js
@@ -2,50 +2,51 @@
 * Server is a .net executable written in js, compiled to .net using jsc
 */
 import System;
+import System.Threading;
 import System.Net;
 import System.IO;
 
 var http = {};
 
-class HttpServer {
+class HttpServer 
+{
+	var httpListener : HttpListener;
+	var prefix;
 
-var httpListener : HttpListener;
-var prefix;
+	function HttpServer( prefix ) {
+		var httpListener : HttpListener = new HttpListener();
+		this.httpListener = httpListener;
+		this.prefix = prefix;
+		httpListener.Prefixes.Add( prefix );
+		// return this;
+	}
 
-function HttpServer( prefix ) {
-	var httpListener : HttpListener = new HttpListener();
-	this.httpListener = httpListener;
-	this.prefix = prefix;
-	httpListener.Prefixes.Add( prefix );
-	// return this;
-}
+	function listen() {
+		print( 'HttpServer.listen():' );
+		this.httpListener.Start();
+		print( 'listener started successfully' );
+		print( 'httpListener: ' + this.httpListener );
+		var result : IAsyncResult = this.httpListener.BeginGetContext( ListenerCallback, httpListener );
+		print( 'BeginGetContext called successfully' );
+		Thread.Sleep( Timeout.Infinite );
+	}
+	function ListenerCallback( result : IAsyncResult ) {
+		print( 'ListenerCallback():' );
+		var listener : HttpListener = HttpListener( result.AsyncState );
+		var context : HttpListenerContext = listener.EndGetContext( result );
+		var request : HttpListenerRequest = context.Request;
+		var response : HttpListenerResponse = context.Response;
+		var responseString : String = "<HTML><BODY> Hello world!</BODY></HTML>";
+		var buffer : byte[] = System.Text.Encoding.UTF8.GetBytes( responseString );
+		response.ContentLength64 = buffer.Length;
+		var output : Stream = response.OutputStream;
+		output.Write( buffer,0,buffer.Length );
+		output.Close();
+	}
 
-function ListenerCallback( result : IAsyncResult ) : void {
-	// print( 'ListenerCallback():' );
-}
-
-// doesn't work because member vars can't hold functions for some reason.
-// var ListenerCallback : Function = function( result: IAsyncResult ) { print( 'ListenerCallback():' ); };
-
-function listen() {
-	print( 'HttpServer.listen():' );
-	this.httpListener.Start();
-	print( 'listener started successfully' );
-	print( 'httpListener: ' + this.httpListener );
-	var result : IAsyncResult = this.httpListener.BeginGetContext( new AsyncCallback( this.ListenerCallback ), httpListener );
-	print( 'BeginGetContext called successfully' );
-}
 } // class
 
-/*
-Non working solution for Async callback
-var _ListenerCallback = function( result : IAsyncResult ) {
-	print( 'ListenerCallback():' );
-}
-var ListenerCallback : AsyncCallback = AsyncCallback( _ListenerCallback );
-*/
-
-http.createServer = function( request_listener, prefix ) {
+http.createServer = function( callback, prefix ) {
 	return new HttpServer( prefix );
 }
 
