b24ef805dad9d2d97fb69338c0de49346d311f91
dnewcome <djn125@yahoo.com>
2010-05-05 15:17:46 -0400

    got request and response api working for simple response

--- server.js
+++ server.js
@@ -40,10 +40,65 @@ class HttpServer
 		var context : HttpListenerContext = listener.EndGetContext( result );
 		var request : HttpListenerRequest = context.Request;
 		var response : HttpListenerResponse = context.Response;
-		this.requestCallback( request, response );
+		
+		// var httpServerRequest = new HttpServerRequest( request );
+		var httpServerResponse = new HttpServerResponse( response );
+		
+		this.requestCallback( request, httpServerResponse );
 	}
 
 } // class
+
+
+// wraps .net request with node.js interface
+class HttpServerRequest 
+{
+	var dataCallbacks = [];
+	var endCallbacks = [];
+	
+	function addListener( eventname, callback) {
+		if( eventname == 'data' ) {
+			dataCallbacks.push( callback );
+		}
+		else if( eventname == 'end' ) {
+			endCallbacks.push( callback );
+		}
+		else {
+			throw "addListener called for unsupported event";
+		}
+
+	}
+}
+
+class HttpServerResponse
+{
+	var httpResponse : HttpListenerResponse;
+	
+	function HttpServerResponse( httpResponse ) {
+		this.httpResponse = httpResponse;
+	}
+	
+	function writeHead( statusCode, headers ) {
+	
+	}
+	
+	function end() {
+		var output : Stream = httpResponse.OutputStream;
+		output.Close();
+	}
+	
+	// default encoding is UTF8
+	function write( chunk, encoding ) {
+		// TODO: not sure how .net handles js strings. everything works if we 
+		// don't explicitly type chunkString, but I'm not sure about things yet
+		var chunkString : String = chunk;
+		var buffer : byte[] = System.Text.Encoding.UTF8.GetBytes( chunkString );
+		httpResponse.ContentLength64 += buffer.Length;
+		
+		var output : Stream = httpResponse.OutputStream;
+		output.Write( buffer, 0, buffer.Length );
+	}
+}
 
 http.createServer = function( callback ) {
 	return new HttpServer( callback );
--- test.js
+++ test.js
@@ -1,12 +1,7 @@
 function cb( request, response ) {
-	print( 'request callback fired' );
-	var responseString : String = "<HTML><BODY> Hello world!</BODY></HTML>";
-		var buffer : byte[] = System.Text.Encoding.UTF8.GetBytes( responseString );
-		response.ContentLength64 = buffer.Length;
-		var output : Stream = response.OutputStream;
-		output.Write( buffer, 0, buffer.Length );
-		
-		output.Close();
+	
+	response.write( '<html><body> Hello world!</body></html>' );
+	response.end();
 }
 
 var server = http.createServer( cb ).listen( 9980, 'localhost' );
