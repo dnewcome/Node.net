b5009f567a27881d58805ddaff32c14500633752
dnewcome <djn125@yahoo.com>
2010-05-07 19:05:29 -0400

    name change of built binary

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-C:\Windows\Microsoft.NET\Framework\v2.0.50727\jsc server.js httpserver.js netserver.js
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\jsc /out:node.exe server.js httpserver.js netserver.js
