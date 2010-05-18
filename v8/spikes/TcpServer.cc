fb5b92da1fd40d421a41f73feb876df0ce0ee8e4
dnewcome <djn125@yahoo.com>
2010-05-18 01:23:47 -0400

    callback registration working

--- v8/spikes/TcpServer.cc
+++ v8/spikes/TcpServer.cc
@@ -20,10 +20,15 @@ public:
 		listeningCallbacks = new std::vector<Handle<Function>>();
 		connectionCallbacks = new std::vector<Handle<Function>>();
 		closeCallbacks = new std::vector<Handle<Function>>();
-		listeningCallbacks->push_back( in_callback );
+		// listeningCallbacks->push_back( in_callback );
+		AddListener( "listening", in_callback );
 	}
 	
-	NetServer() {}
+	NetServer() {
+		listeningCallbacks = new std::vector<Handle<Function>>();
+		connectionCallbacks = new std::vector<Handle<Function>>();
+		closeCallbacks = new std::vector<Handle<Function>>();
+	}
 	
 	void Listen( System::Int32 port, System::String^ host ) {
 		
@@ -33,18 +38,61 @@ public:
 		this->tcpListener = gcnew TcpListener( ipAddress, port );
 		// print( 'net.Server.listen(): starting tcp server' );
 		this->tcpListener->Start();
+		
 		// queueWorkItem( { callback: raiseListeningEvent, args: [] } );
+		RaiseListeningEvent();
 		
 		System::AsyncCallback^ ListenerCallbackDelegate = gcnew System::AsyncCallback( this, &NetServer::ListenerCallback );
-		
 		this->tcpListener->BeginAcceptTcpClient( ListenerCallbackDelegate, NULL );
 	}
 	
-	
-	
 	void ListenerCallback( System::IAsyncResult^ result ) {
 		printf("%s\n", "listener called" );
+		System::AsyncCallback^ ListenerCallbackDelegate = gcnew System::AsyncCallback( this, &NetServer::ListenerCallback );
+		this->tcpListener->BeginAcceptTcpClient( ListenerCallbackDelegate, NULL );
 		
+		TcpClient^ client = this->tcpListener->EndAcceptTcpClient( result );
+		System::IO::Stream^ stream = client->GetStream();
+		//queueWorkItem( { callback: raiseConnectionEvent, args: [ stream ] } );
+		RaiseConnectionEvent();
+		
+		// kick off async read
+		// stream.read();
+		System::IO::StreamReader^ sr = gcnew System::IO::StreamReader( stream );
+		System::String^ data = sr->ReadToEnd();
+		System::Console::WriteLine( data );
+		
+	}
+	
+	void RaiseListeningEvent() {
+		printf( "%s\n", "http.server.raiseListeningEvent()" );
+		printf( "http.server.raiseListeningEvent(): calling %i callbacks\n", (int)this->listeningCallbacks->size() );
+		for( int i=0; i < listeningCallbacks->size(); i++ ) {
+			Handle<Function> fn = listeningCallbacks->at(i);
+			// fn->Call();
+		}
+	}
+	void RaiseConnectionEvent() {
+		printf( "%s\n", "http.server.RaiseConnectionEvent()" );
+		for( int i=0; i < connectionCallbacks->size(); i++ ) {
+			Handle<Function> fn = connectionCallbacks->at(i);
+			// fn->Call();
+		}
+	}
+	
+	void AddListener( System::String^ eventname, Handle<Function> callback ) {
+		if( eventname == "listening" ) {
+			listeningCallbacks->push_back( callback );
+		}
+		else if( eventname == "connection" ) {
+			connectionCallbacks->push_back( callback );
+		}
+		else if( eventname == "close" ) {
+			closeCallbacks->push_back( callback );
+		}
+		else {
+			throw "addListener called for unsupported event";
+		}
 	}
 };
 
@@ -60,7 +108,16 @@ int main(int argc, char* argv[]) {
 	
 	printf("%s\n", "working" );
 	
-	NetServer^ netServer = gcnew NetServer();
+	// create global and context
+	v8::Handle<v8::ObjectTemplate> global = v8::ObjectTemplate::New();
+	v8::Handle<v8::Context> context = v8::Context::New(NULL, global);
+	v8::Context::Scope context_scope(context);
+
+	// note: GetFunction() fails if there is no context 
+	Local<FunctionTemplate> cbTemplate = FunctionTemplate::New();
+	Local<Function> cbFunction = cbTemplate->GetFunction();
+	
+	NetServer^ netServer = gcnew NetServer( cbFunction );
 	netServer->Listen( 9980, "localhost" );
 	Thread::Sleep( Timeout::Infinite );
 }
