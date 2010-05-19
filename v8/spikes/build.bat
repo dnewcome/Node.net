b2928a1d254c1bf277ca7e868693468d3c9ffcf5
dnewcome <djn125@yahoo.com>
2010-05-19 18:03:16 -0400

    added /DUSING_V8_SHARED to solve link errors that I was getting

--- v8/spikes/build.bat
+++ v8/spikes/build.bat
@@ -4,4 +4,4 @@ if "%1"=="env" (
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\bin\vcvars32.bat"
 )
 
-cl /IC:\Users\dan\Desktop\sandbox\v8\include  /clr %1 ..\v8.lib wsock32.lib winmm.lib ws2_32.lib
+cl /DUSING_V8_SHARED /IC:\Users\dan\Desktop\sandbox\v8\include  /clr %1 ..\v8.lib wsock32.lib winmm.lib ws2_32.lib
