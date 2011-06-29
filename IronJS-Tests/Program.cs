ea00951788b2dd04f49a684d1f2a7d0c7b195237
dnewcome <djn125@yahoo.com>
2011-06-29 14:40:02 -0700

    added actual test project - missed this last time

---
+++ IronJS-Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronJS_FS_Tests
+{
+	class Program
+	{
+		static void Main( string[] args ) {
+			Server instance = new Server();
+			instance.evalCommandlineArgument();
+			instance.runEventLoop();			
+		}
+	}
+}
