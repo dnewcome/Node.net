3d383281b7d16bbb5ea95cbd4701335fb95da1ab
dnewcome <djn125@yahoo.com>
2011-06-30 00:08:01 -0700

    impelemented removeAllListeners, emit() and on()

--- IronJS/EventEmitter.cs
+++ IronJS/EventEmitter.cs
@@ -12,5 +12,40 @@ namespace Node.Net
 
 		public EventEmitter( IronJS.Environment env ) 
 			: base( env, env.Maps.Base, env.Prototypes.Object ) { }
-	}
-}
+
+		public void emit( string eventname, params IronJS.BoxedValue[] args ) {
+			foreach( object callback in Callbacks[eventname] ) {
+				log.Trace( "calling js function callback: " + eventname );
+				IronJS.FunctionObject func = ( IronJS.FunctionObject )callback;
+				func.Call( this, args );
+			}
+		}
+
+		public void on( string eventname, IronJS.FunctionObject callback ) {
+			log.Trace( "EventEmitter - adding listener: " + eventname );
+			// TODO: move listener counts to static field on the emitter
+			Server.instance.Listeners++;
+			// TODO: listen is not an event .. remove this
+			if( Callbacks.ContainsKey( eventname ) ) {
+				Callbacks[ eventname ].Add( callback );
+			}
+			// TODO: I think that node allows any event to be registered, 
+			// so it probably isn't correct to throw on unexpected event
+			else {
+				throw new Exception( "on called for unsupported event" );
+			}
+		}
+
+		public void removeAllListeners( string eventname ) {
+			if( Callbacks.ContainsKey( eventname ) ) {
+				// TODO: move listener counts to static field on the emitter
+				Server.instance.Listeners -= Callbacks[ eventname ].Count;
+				Callbacks[ eventname ].Clear();
+			}
+			else {
+				throw new Exception( "tried to remove invalid event handlers: " + eventname );
+			}
+		}
+
+	} // class
+} // namespace 
