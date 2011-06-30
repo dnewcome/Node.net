af1bef9eb7ea7332052d2b2fd3abb6d12db825ea
dnewcome <djn125@yahoo.com>
2011-06-29 21:38:24 -0700

    Moved NetStream into its own file finally.

--- IronJS/netserver.cs
+++ IronJS/netserver.cs
@@ -15,119 +15,6 @@ using IronJS.Native;
 */
 namespace Node.Net 
 {
-	public class NetStream : IronJS.CommonObject
-	{
-		// XXX making stream public to work around incomplete design - fix this
-		public Stream stream;
-
-		// TODO: callbacks will be JS functions, should probably use 
-		// List<IFunction>
-		ArrayList dataCallbacks = new ArrayList();
-		ArrayList endCallbacks = new ArrayList();
-		
-		// TODO: this should be defined somewhere else
-		// Using 1k buffer - experimentally, seems to be what node.js uses
-		int bufferSize = 1024; 
-		
-		public NetStream( Stream stream, IronJS.Environment env ) : base( env, env.Maps.Base, env.Prototypes.Object ) {
-			this.stream = stream;
-			var objMethod = Utils.CreateFunction<Action<string, IronJS.FunctionObject>>( env, 0, addListener );
-			this.Put( "addListener", objMethod, DescriptorAttrs.Immutable );
-
-			var objRemoveMethod = Utils.CreateFunction<Action<string>>( env, 0, removeAllListeners );
-			this.Put( "removeAllListeners", objRemoveMethod, DescriptorAttrs.Immutable );
-		}
-
-		public void read() {
-			byte[] buffer = new byte[ bufferSize ];
-			this.stream.BeginRead( buffer, 0, bufferSize, ReadCallback, buffer );
-		}
-			
-		public void ReadCallback( IAsyncResult result ) {
-			byte[] buffer = ( byte[] )result.AsyncState;
-			int bytesRead = this.stream.EndRead( result );
-			log.Trace( "ReadCallback(): read " + bytesRead + "bytes from stream" );
-			// TODO: need to implement setEncoding still, all data returned as utf8
-			string chunk = System.Text.Encoding.UTF8.GetString( buffer, 0, bytesRead );
-			
-			if( bytesRead > 0 ) {
-				// queue work before calling another read, could be out of order otw
-				Server.instance.queueWorkItem( new Callback() { name = "stream:raiseDataEvent", callback = raiseDataEvent, args = new object[] { chunk } } );
-				byte[] nextBuffer = new byte[ bufferSize ];
-				this.stream.BeginRead( nextBuffer, 0, bufferSize, ReadCallback, nextBuffer );
-			}
-			else {
-				// TODO: node.js docs say end is raised when FIN is sent by 
-				// remote end of socket, not sure if raising "end" when there
-				// is no more data is the right thing to do. It does the right thing
-				// when using Telnet as the client 
-				Server.instance.queueWorkItem( new Callback() { name = "stream:raiseEndEvent", callback = raiseEndEvent, args = new object[]{} } );
-			}
-		}
-
-		// TODO: encoding is assumed to be utf8, need to implement encoding types
-		public void write( string chunk ) {
-			byte[] buffer = System.Text.Encoding.UTF8.GetBytes( chunk );
-			this.stream.Write( buffer, 0, buffer.Length );
-		}
-		
-		public void end() {
-            log.Trace( "closing stream" );
-			this.stream.Close();
-		}
-		public void addListener( string eventname, IronJS.FunctionObject callback ) {
-			Server.instance.Listeners++;
-			log.Trace( "NetStream: adding listener: " + eventname );
-			if( eventname == "data" ) {
-				dataCallbacks.Add( callback );
-			}
-			else if( eventname == "end" ) {
-				endCallbacks.Add( callback );
-			}
-		}
-		public void removeAllListeners( string eventname ) {
-			if( eventname == "data" ) {
-				Server.instance.Listeners -= dataCallbacks.Count;
-				dataCallbacks.Clear();
-			}
-			else if( eventname == "end" ) {
-				Server.instance.Listeners -= endCallbacks.Count;
-				endCallbacks.Clear();
-			}
-		}
-		
-		public void raiseDataEvent( object[] args ) {
-			string chunk = ( string )args[0];
-			log.Trace( "net.stream.raiseDataEvent():" + chunk );
-			for( var i=0; i < dataCallbacks.Count; i++ ) {
-				// ( ( IronJS.FunctionObject )dataCallbacks[i] ).Call( this, new object[] { chunk } );
-				IronJS.FunctionObject func = ( IronJS.FunctionObject )dataCallbacks[i];
-				/*
-				Action<IronJS.FunctionObject,IronJS.CommonObject,string> fun = 
-					func.Compiler.compileAs<Action<IronJS.FunctionObject,IronJS.CommonObject,string>>(func);
-				// fun.Invoke(func, func.Env.Globals, new object[] {chunk} );
-				log.Trace( "invoking JS raiseDataEvent callback" );
-				fun.Invoke(func, this, chunk );
-				 */
-				func.Call<string>( this, chunk );
-			}
-		}
-		
-		public void raiseEndEvent( object[] args ) {
-			log.Trace( "net.stream.raiseEndEvent()" );
-			for( var i=0; i < endCallbacks.Count; i++ ) {
-				// ( ( IFunction )endCallbacks[i] ).Call( this, new object[] {} );
-				IronJS.FunctionObject func = ( IronJS.FunctionObject )endCallbacks[i];
-				/*
-				Action<IronJS.FunctionObject,IronJS.CommonObject,object[]> fun = 
-					func.Compiler.compileAs<Action<IronJS.FunctionObject,IronJS.CommonObject,object[]>>(func);
-				fun.Invoke(func, func.Env.Globals, new object[] {} );
-				 */
-				func.Call( this );
-			}
-		}
-		
-	} // class
 
 	public class NetServer : IronJS.CommonObject
 	{
@@ -138,11 +25,7 @@ namespace Node.Net
 		ArrayList closeCallbacks = new ArrayList();
 
 		public NetServer( IronJS.FunctionObject callback, IronJS.Environment env ) : base( env, env.Maps.Base, env.Prototypes.Object ) {
-			// have to set this stuff up. not sure why yet
-			
-			// Prototype = context.ObjectConstructor.Object_prototype;
-			// Class = ObjClass.Array;
-
+		
 			log.Trace( "creating NetServer" );
 			
 			var objMethod = Utils.createHostFunction<Func<object, string, CommonObject>>(env, listen);
@@ -185,13 +68,7 @@ namespace Node.Net
 		public void raiseListeningEvent( object[] args ) {
 			log.Trace( "http.server.raiseListeningEvent()" );
 			for( var i=0; i < listeningCallbacks.Count; i++ ) {
-				// ( ( IFunction )listeningCallbacks[i] ).Call( this, new object[]{} );
 				IronJS.FunctionObject func = ( IronJS.FunctionObject )listeningCallbacks[i];
-				/*
-				Action<IronJS.FunctionObject,IronJS.CommonObject,object[]> fun = 
-					func.Compiler.compileAs<Action<IronJS.FunctionObject,IronJS.CommonObject,object[]>>(func);
-				fun.Invoke(func, func.Env.Globals, new object[] {} );
-				*/
 				func.Call( this );
 			}
 		}
@@ -202,13 +79,7 @@ namespace Node.Net
 			
 			foreach( object callback in connectionCallbacks ) {
 				log.Trace( "calling js function callback" );
-				// ( ( IFunction )callback ).Call( this, new object[] { stream } );
 				IronJS.FunctionObject func = ( IronJS.FunctionObject )callback;
-				/*
-				Action<IronJS.FunctionObject,IronJS.CommonObject,IronJS.CommonObject> fun = 
-					func.Compiler.compileAs<Action<IronJS.FunctionObject,IronJS.CommonObject,IronJS.CommonObject>>(func);
-				fun.Invoke(func, this, stream );
-				*/
 				func.Call<NetStream>( this, stream );
 			}
 		}
