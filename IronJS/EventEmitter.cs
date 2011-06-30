94f9248708ffd2c8ded8ad7b1f29d6b11426ea2b
dnewcome <djn125@yahoo.com>
2011-06-29 23:34:47 -0700

    moving some functionality to EventEmitter - refactoring the callbacks to Dictionary data types

---
+++ IronJS/EventEmitter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Node.Net
+{
+	public class EventEmitter : IronJS.CommonObject
+	{
+		public Dictionary<string, ArrayList> Callbacks { get; set; }
+
+		public EventEmitter( IronJS.Environment env ) 
+			: base( env, env.Maps.Base, env.Prototypes.Object ) { }
+	}
+}
--- IronJS/netserver.cs
+++ IronJS/netserver.cs
@@ -2,6 +2,7 @@ using System.Net.Sockets;
 using System.Net;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 using IronJS;
@@ -16,16 +17,20 @@ using IronJS.Native;
 namespace Node.Net 
 {
 
-	public class NetServer : IronJS.CommonObject
+	public class NetServer : EventEmitter
 	{
 		TcpListener tcpServer;
 
-		ArrayList listeningCallbacks = new ArrayList();
-		ArrayList connectionCallbacks = new ArrayList();
-		ArrayList closeCallbacks = new ArrayList();
-
-		public NetServer( IronJS.FunctionObject callback, IronJS.Environment env ) : base( env, env.Maps.Base, env.Prototypes.Object ) {
 		
+		public NetServer( IronJS.FunctionObject callback, IronJS.Environment env ) : base( env ) {
+
+			// TODO: move as much of this as possible to EventEmitter
+			Callbacks = new Dictionary<string, ArrayList>() { 
+				// { "listen", new ArrayList() },
+				{ "connect", new ArrayList() },
+				{ "close", new ArrayList() }
+			};
+
 			log.Trace( "creating NetServer" );
 			
 			var objMethod = Utils.createHostFunction<Func<object, string, CommonObject>>(env, listen);
@@ -34,7 +39,7 @@ namespace Node.Net
 
 			this.Put( "listen", objMethod, TypeTags.Function );
 			this.Put( "removeAllListeners", removeAllListenersMethod, TypeTags.Function );
-			this.addListener( "connection", callback );
+			this.addListener( "connect", callback );
 		}
 		
 		// in Node.js, listen is async, here it blocks on call to Start()
@@ -45,7 +50,9 @@ namespace Node.Net
 			this.tcpServer = new TcpListener( ipAddress, port );
 			log.Trace( "net.Server.listen(): starting tcp server" );
 			this.tcpServer.Start();
-			Server.instance.queueWorkItem( new Callback { callback = raiseListeningEvent, args = new object[]{} } );
+			
+			// TODO: 'listening' is not an event. Slated for removal.
+			// Server.instance.queueWorkItem( new Callback { callback = raiseListeningEvent, args = new object[]{} } );
 			this.tcpServer.BeginAcceptTcpClient( listenerCallback, null );
 			return this;
 		}
@@ -65,19 +72,24 @@ namespace Node.Net
 		}
 		
 		// TODO: pull this stuff out to an event class
+
+		// TODO: 'listen/listening' is not an event. Not sure why this is in here
+		// slated for removal.
+		/*
 		public void raiseListeningEvent( object[] args ) {
 			log.Trace( "http.server.raiseListeningEvent()" );
-			for( var i=0; i < listeningCallbacks.Count; i++ ) {
-				IronJS.FunctionObject func = ( IronJS.FunctionObject )listeningCallbacks[i];
+			for( var i=0; i < Callbacks["listen"].Count; i++ ) {
+				IronJS.FunctionObject func = ( IronJS.FunctionObject )Callbacks["listen"][i];
 				func.Call( this );
 			}
 		}
-		
+		*/
+
 		public void raiseConnectionEvent( object[] args ) {
 			NetStream stream = ( NetStream )args[0];
-			log.Trace( "http.server.raiseConnectionEvent() - calling " + connectionCallbacks.Count + " callbacks" );
-			
-			foreach( object callback in connectionCallbacks ) {
+			log.Trace( "http.server.raiseConnectionEvent() - calling " + Callbacks["connect"].Count + " callbacks" );
+
+			foreach( object callback in Callbacks["connect"] ) {
 				log.Trace( "calling js function callback" );
 				IronJS.FunctionObject func = ( IronJS.FunctionObject )callback;
 				func.Call<NetStream>( this, stream );
@@ -85,31 +97,27 @@ namespace Node.Net
 		}
 
 		public void removeAllListeners( string eventname ) {
-			if( eventname == "listening" ) {
-				Server.instance.Listeners -= listeningCallbacks.Count;
-				listeningCallbacks.Clear();
+			if( Callbacks.ContainsKey( eventname ) ) {
+				Server.instance.Listeners -= Callbacks[ eventname ].Count;
+				Callbacks[ eventname ].Clear();
 			}
-			else if( eventname == "connection" ) {
-				Server.instance.Listeners -= connectionCallbacks.Count;
-				connectionCallbacks.Clear();
-			}
-			else if( eventname == "close" ) {
-				Server.instance.Listeners -= closeCallbacks.Count;
-				closeCallbacks.Clear();
+			else {
+				throw new Exception( "tried to remove invalid event handlers: " + eventname );
 			}
 		}
 
 		public void addListener( string eventname, IronJS.FunctionObject callback) {
 			log.Trace( "NetServer - adding listener: " + eventname );
 			Server.instance.Listeners++;
-			if( eventname == "listening" ) {
-				listeningCallbacks.Add( callback );
+			// TODO: listen is not an event .. remove this
+			if( eventname == "listen" ) {
+				Callbacks["listen"].Add( callback );
 			}
-			else if( eventname == "connection" ) {
-				connectionCallbacks.Add( callback );
+			else if( eventname == "connect" ) {
+				Callbacks["connect"].Add( callback );
 			}
 			else if( eventname == "close" ) {
-				closeCallbacks.Add( callback );
+				Callbacks["close"].Add( callback );
 			}
 			else {
 				throw new Exception( "addListener called for unsupported event" );
